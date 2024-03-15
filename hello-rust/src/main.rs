f037e7b2ad1404c36aa683ec3188ed49ffff9404
Zach MacConnell <zachary.macconnell@gmail.com>
2024-03-15 13:12:31 -0600

    Finished basic calculator

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,130 +1,62 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
-use std::fmt;
-
+use std::io;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello from Rust!");
-    let width = message.chars().count();
+    println!("-- Calculator --");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+    loop {
+        println!("Please enter your expression (e.g., 2 + 2), or type 'quit' to exit:");
 
-    demo_1()
-}
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Problem reading line");
 
+        let input = input.trim();
 
-fn demo_1() {
-    // In general, the `{}` will be automatically replaced with any
-    // arguments. These will be stringified.
-    println!("{} days", 31);
+        if input == "quit" {
+            println!("Closing...");
+            break;
+        }
 
-    // Positional arguments can be used. Specifying an integer inside `{}`
-    // determines which additional argument will be replaced. Arguments start
-    // at 0 immediately after the format string.
-    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+        let tokens: Vec<&str> = input.split_whitespace().collect();
 
-    // As can named arguments.
-    println!("{subject} {verb} {object}",
-             object="the lazy dog",
-             subject="the quick brown fox",
-             verb="jumps over");
+        if tokens.len() != 3 {
+            println!("Invalid expression. Please enter an expression in the format 'number operator number'.");
+            continue;
+        }
 
-    // Different formatting can be invoked by specifying the format character
-    // after a `:`.
-    println!("Base 10:               {}",   69420); // 69420
-    println!("Base 2 (binary):       {:b}", 69420); // 10000111100101100
-    println!("Base 8 (octal):        {:o}", 69420); // 207454
-    println!("Base 16 (hexadecimal): {:x}", 69420); // 10f2c
-    println!("Base 16 (hexadecimal): {:X}", 69420); // 10F2C
+        let num1: f64 = match tokens[0].parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid number: {}", tokens[0]);
+                continue;
+            }
+        };
 
-    // You can right-justify text with a specified width. This will
-    // output "    1". (Four white spaces and a "1", for a total width of 5.)
-    println!("{number:>5}", number=1);
+        let num2: f64 = match tokens[2].parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid number: {}", tokens[2]);
+                continue;
+            }
+        };
 
-    // You can pad numbers with extra zeroes,
-    println!("{number:0>5}", number=1); // 00001
-    // and left-adjust by flipping the sign. This will output "10000".
-    println!("{number:0<5}", number=1); // 10000
+        let result = match tokens[1] {
+            "+" => num1 + num2,
+            "-" => num1 - num2,
+            "*" => num1 * num2,
+            "/" => {
+                if num2 == 0.0 {
+                    println!("Error: Division by zero");
+                    continue;
+                } else {
+                    num1 / num2
+                }
+            }
+            _ => {
+                println!("Invalid operator: {}", tokens[1]);
+                continue;
+            }
+        };
 
-    // You can use named arguments in the format specifier by appending a `$`.
-    println!("{number:0>width$}", number=1, width=5);
-
-    // Rust even checks to make sure the correct number of arguments are used.
-    println!("My name is {0}, {1} {0}", "Bond", "James");
-    // FIXME ^ Add the missing argument: "James"
-
-    // Only types that implement fmt::Display can be formatted with `{}`. User-
-    // defined types do not implement fmt::Display by default.
-
-    #[allow(dead_code)] // disable `dead_code` which warn against unused module
-    struct Structure(i32);
-
-    // This will not compile because `Structure` does not implement
-    // fmt::Display.
-    // println!("This struct `{}` won't print...", Structure(3));
-    // TODO ^ Try uncommenting this line
-
-    // For Rust 1.58 and above, you can directly capture the argument from a
-    // surrounding variable. Just like the above, this will output
-    // "    1", 4 white spaces and a "1".
-    let number: f64 = 1.0;
-    let width: usize = 5;
-    println!("{number:>width$}");
-}
-
-
-// A structure holding two numbers. `Debug` will be derived so the results can
-// be contrasted with `Display`.
-#[derive(Debug)]
-struct MinMax(i64, i64);
-
-// Implement `Display` for `MinMax`.
-impl fmt::Display for MinMax {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Use `self.number` to refer to each positional data point.
-        write!(f, "({}, {})", self.0, self.1)
+        println!("Result: {}", result);
     }
 }
-
-// Define a structure where the fields are nameable for comparison.
-#[derive(Debug)]
-struct Point2D {
-    x: f64,
-    y: f64,
-}
-
-// Similarly, implement `Display` for `Point2D`.
-impl fmt::Display for Point2D {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Customize so only `x` and `y` are denoted.
-        write!(f, "x: {}, y: {}", self.x, self.y)
-    }
-}
-
-fn demo_2() {
-    let minmax = MinMax(0, 14);
-
-    println!("Compare structures:");
-    println!("Display: {}", minmax);
-    println!("Debug: {:?}", minmax);
-
-    let big_range =   MinMax(-300, 300);
-    let small_range = MinMax(-3, 3);
-
-    println!("The big range is {big} and the small is {small}",
-             small = small_range,
-             big = big_range);
-
-    let point = Point2D { x: 3.3, y: 7.2 };
-
-    println!("Compare points:");
-    println!("Display: {}", point);
-    println!("Debug: {:?}", point);
-
-    // Error. Both `Debug` and `Display` were implemented, but `{:b}`
-    // requires `fmt::Binary` to be implemented. This will not work.
-    // println!("What does Point2D look like in binary: {:b}?", point);
-}
-
